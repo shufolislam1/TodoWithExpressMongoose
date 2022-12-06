9fd463a5f165ae6388ae6175d40e9e33d8749c53
shufol islam <shufolislam1255@gmail.com>
2022-12-06 11:55:10 +0600

    signup completed with hashed password

--- routeHandler/userHandler.js
+++ routeHandler/userHandler.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 const userSchema = require('../schemas/userSchema')
 const router = express.Router();
@@ -6,8 +7,23 @@ const router = express.Router();
 const User = new mongoose.model("User", userSchema)
 
 // signup
-router.post('/', async(req, res) => {
-
+router.post('/signup', async(req, res) => {
+    try{
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const newUser = new User({
+            name: req.body.name,
+            userName: req.body.userName,
+            password: hashedPassword,
+        });
+        await newUser.save();
+        res.status(200).json({
+            message: "Signup successfull!!!",
+        })
+    }catch{
+        res.status(500).json({
+            message: "Signup Failed!!!",
+        })
+    }
 })
 
 
